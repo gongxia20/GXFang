42173cef0869a94a9507343c009dd52f37b15318
James <gongxia20@sina.cn>
2018-11-16 14:36:47 +0800

    改了baseVc

--- GXFang/Classes/Base(基类)/GXBaseViewController.m
+++ GXFang/Classes/Base(基类)/GXBaseViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "GXBaseViewController.h"
+#import "MainViewController.h"
 
 @interface GXBaseViewController ()
 
@@ -18,6 +19,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor redColor];
+    
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 200, 200)];
+    btn.backgroundColor = [UIColor whiteColor];
+    [btn setTitle:@"按钮文字" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+}
+
+- (void)btnClick:(UIButton *)btn {
+//    NSLog(@"-----");
+    GXBaseViewController *vc = [GXBaseViewController new];
+    vc.hidesBottomBarWhenPushed = YES;
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 @end
