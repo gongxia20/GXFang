82a0a5d35d43e18d037052a9e1cb6e396e381592
James <gongxia20@sina.cn>
2018-11-17 19:22:36 +0800

    添加home中代码

--- GXFang/Classes/Home(首页)/Controller/GXHomeViewController.m
+++ GXFang/Classes/Home(首页)/Controller/GXHomeViewController.m
@@ -8,7 +8,12 @@
 
 #import "GXHomeViewController.h"
 
-@interface GXHomeViewController ()
+@interface GXHomeViewController () <UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout>
+
+@property (nonatomic, strong) id dataManager;
+@property (nonatomic, strong) NSArray *dataArray;
+@property (nonatomic, strong) UIView *tabSegmentedControl;
+@property (nonatomic, strong) UICollectionView *collectionView;
 
 @end
 
@@ -18,6 +23,47 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor orangeColor];
+    
+    self.tabSegmentedControl = [UIView new];
+    [self.view addSubview:self.tabSegmentedControl];
+    
+    self.collectionView = [UICollectionView new];
+    [self.view addSubview:self.collectionView];
 }
+
+#pragma mark -- collectionView datasource
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return _dataArray.count;
+}
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    return cell;
+}
+
+
+# pragma mark -- UICollectionViewDelegateFlowLayout
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(collectionView.bounds.size.width, collectionView.bounds.size.height);
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return UIEdgeInsetsZero;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return 0;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 0;
+}
+
+# pragma mark --collectionView delegate
+//- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+//
+//}
 
 @end
