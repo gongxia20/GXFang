09f6630e2f2877a106b01a4744ec57542c693615
James <gongxia20@sina.cn>
2018-11-24 16:37:36 +0800

    xx

--- GXFang/Classes/Home(首页)/Controller/GXHomeCollectionViewProxy.m
+++ GXFang/Classes/Home(首页)/Controller/GXHomeCollectionViewProxy.m
@@ -10,4 +10,21 @@
 
 @implementation GXHomeCollectionViewProxy
 
+# pragma mark -- UICollectionViewDelegateFlowLayout
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(collectionView.bounds.size.width, collectionView.bounds.size.height);
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return UIEdgeInsetsZero;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return 0;
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section {
+    return 0;
+}
+
 @end
